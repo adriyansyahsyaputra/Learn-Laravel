f4940e28c5d326b1428059f0995a716fc1253a5f
Adriyansyah Syaputra <adriansyahputra2626@gmail.com>
2024-02-22 20:12:48 +0700

    first day learning to use the laravel framework with WPU youtube guide

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home | ASN</title>
+</head>
+<body>
+    
+    <h1>Ini adalah halaman home</h1>
+
+</body>
+</html>
