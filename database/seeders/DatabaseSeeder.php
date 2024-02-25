899a5f552c48c1e293e43680b9e4f5335d39b97a
Adriyansyah Syaputra <adriansyahputra2626@gmail.com>
2024-02-25 15:23:36 +0700

    Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,73 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Adriyansyah Syaputra',
+            'email' => 'adriyansyah@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Syaputra Nasution',
+            'email' => 'nasution@gmail.com',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Business',
+            'slug' => 'business'
+        ]);
+
+        Post::create ([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores, itaque ullam voluptate amet, magni quidem quibusdam ipsam inventore, delectus suscipit saepe vero sit officiis doloremque veniam veritatis velit possimus tempore illo. Blanditiis totam fugit, deserunt aut voluptates rem ex mollitia! Distinctio modi voluptatibus dolore quod consectetur voluptatem nihil provident neque! Expedita nam ut maxime nisi odio error ullam delectus vitae officiis, vel nemo temporibus nihil blanditiis, molestiae quae id explicabo quaerat cumque repellendus eligendi minima doloremque repudiandae culpa accusamus. Porro facilis impedit est labore esse natus ab consequuntur, sequi cum, sed sunt magni excepturi aliquam explicabo molestias odio facere quidem quis eveniet ipsam soluta tempora exercitationem dicta iure. Nam iure hic nemo sunt accusamus nihil, id tempora quam. Dolorem, itaque fuga distinctio velit nam enim obcaecati eaque ullam soluta provident totam ratione earum nisi deserunt ipsum quis repellat tenetur, ad eos vel ea est. Dolorum fugiat asperiores earum laborum!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create ([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores, itaque ullam voluptate amet, magni quidem quibusdam ipsam inventore, delectus suscipit saepe vero sit officiis doloremque veniam veritatis velit possimus tempore illo. Blanditiis totam fugit, deserunt aut voluptates rem ex mollitia! Distinctio modi voluptatibus dolore quod consectetur voluptatem nihil provident neque! Expedita nam ut maxime nisi odio error ullam delectus vitae officiis, vel nemo temporibus nihil blanditiis, molestiae quae id explicabo quaerat cumque repellendus eligendi minima doloremque repudiandae culpa accusamus. Porro facilis impedit est labore esse natus ab consequuntur, sequi cum, sed sunt magni excepturi aliquam explicabo molestias odio facere quidem quis eveniet ipsam soluta tempora exercitationem dicta iure. Nam iure hic nemo sunt accusamus nihil, id tempora quam. Dolorem, itaque fuga distinctio velit nam enim obcaecati eaque ullam soluta provident totam ratione earum nisi deserunt ipsum quis repellat tenetur, ad eos vel ea est. Dolorum fugiat asperiores earum laborum!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+        Post::create ([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores, itaque ullam voluptate amet, magni quidem quibusdam ipsam inventore, delectus suscipit saepe vero sit officiis doloremque veniam veritatis velit possimus tempore illo. Blanditiis totam fugit, deserunt aut voluptates rem ex mollitia! Distinctio modi voluptatibus dolore quod consectetur voluptatem nihil provident neque! Expedita nam ut maxime nisi odio error ullam delectus vitae officiis, vel nemo temporibus nihil blanditiis, molestiae quae id explicabo quaerat cumque repellendus eligendi minima doloremque repudiandae culpa accusamus. Porro facilis impedit est labore esse natus ab consequuntur, sequi cum, sed sunt magni excepturi aliquam explicabo molestias odio facere quidem quis eveniet ipsam soluta tempora exercitationem dicta iure. Nam iure hic nemo sunt accusamus nihil, id tempora quam. Dolorem, itaque fuga distinctio velit nam enim obcaecati eaque ullam soluta provident totam ratione earum nisi deserunt ipsum quis repellat tenetur, ad eos vel ea est. Dolorum fugiat asperiores earum laborum!',
+            'category_id' => 3,
+            'user_id' => 2
+        ]);
+
+        Post::create ([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est dolores, itaque ullam voluptate amet, magni quidem quibusdam ipsam inventore, delectus suscipit saepe vero sit officiis doloremque veniam veritatis velit possimus tempore illo. Blanditiis totam fugit, deserunt aut voluptates rem ex mollitia! Distinctio modi voluptatibus dolore quod consectetur voluptatem nihil provident neque! Expedita nam ut maxime nisi odio error ullam delectus vitae officiis, vel nemo temporibus nihil blanditiis, molestiae quae id explicabo quaerat cumque repellendus eligendi minima doloremque repudiandae culpa accusamus. Porro facilis impedit est labore esse natus ab consequuntur, sequi cum, sed sunt magni excepturi aliquam explicabo molestias odio facere quidem quis eveniet ipsam soluta tempora exercitationem dicta iure. Nam iure hic nemo sunt accusamus nihil, id tempora quam. Dolorem, itaque fuga distinctio velit nam enim obcaecati eaque ullam soluta provident totam ratione earum nisi deserunt ipsum quis repellat tenetur, ad eos vel ea est. Dolorum fugiat asperiores earum laborum!',
+            'category_id' => 4,
+            'user_id' => 1
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article>
         <h2>{{ $post->title }}</h2>
         
-        <p>By. Adriyansyah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
         {!! $post->body !!}
 
