d91e8162628203c19a8890a4bdb166706f4a4e61
Adriyansyah Syaputra <adriansyahputra2626@gmail.com>
2024-02-29 13:33:19 +0700

    Create view login & Registration

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-danger">
-    <div class="container">
-        <a class="navbar-brand" href="/">My Website</a>
+    <div class="container text">
+        <a class="navbar-brand text-white" href="/">My Website</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,16 +8,22 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'Home') ? 'active' : '' }}" href="/">Home</a>
+                    <a class="nav-link text-white {{ $active === 'Home' ? 'active' : '' }}" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'about') ? 'active' : '' }}" href="/about">About</a>
+                    <a class="nav-link text-white {{ $active === 'about' ? 'active' : '' }}" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'posts') ? 'active' : '' }}" href="/posts">Blog</a>
+                    <a class="nav-link text-white {{ $active === 'posts' ? 'active' : '' }}" href="/posts">Blog</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'categories') ? 'active' : '' }}" href="/categories">Categories</a>
+                    <a class="nav-link text-white {{ $active === 'categories' ? 'active' : '' }}"
+                        href="/categories">Categories</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="/login" class="nav-link text-white {{ $active === 'login' ? 'active' : '' }}"><i class="bi bi-box-arrow-right"></i>Login</a>
                 </li>
             </ul>
         </div>
